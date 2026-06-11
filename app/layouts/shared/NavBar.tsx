7541c03a4386d622946bd74c5006afa40557ed87
Habibulah Sirat <63021173+habibullahsirat@users.noreply.github.com>
2026-06-11 16:35:11 +0600

    Navbar layout created.

--- app/layouts/shared/NavBar.tsx
+++ app/layouts/shared/NavBar.tsx
@@ -1,8 +1,22 @@
 export default function NavBar() {
   return (
-    <div>
-      <div className="text-[#3D0B37] text-[24px] font-['Acolnica']">
+    <div className="flex justify-between">
+      <div className="text-[#3D0B37] text-[24px] font-['Aclonica']">
         Dazzling Diva
+      </div>
+      <div className="flex justify-between">
+        <div>Home</div>
+        <div>Shop</div>
+        <div>Three Piece</div>
+        <div>Two Piece</div>
+        <div>Hijab</div>
+        <div>New Arrivals</div>
+        <div>Deals & Offers</div>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
     </div>
   );
