ba97a4c0e9270cc3ac55f6acfbed3b19a05df587
Habibulah Sirat <63021173+habibullahsirat@users.noreply.github.com>
2026-06-11 22:04:18 +0600

    Footer design updated.

--- app/layouts/shared/Footer.tsx
+++ app/layouts/shared/Footer.tsx
@@ -1,47 +1,67 @@
 export default function Footer() {
   return (
-    <footer className="bg-[#3D0B37] flex justify-between px-[80px] py-[48px]">
-      <div>
-        <div className="text-[#FFDD00] text-[24px] font-['Aclonica'] mb-[30px]">
-          Dazzling Diva
+    <footer className="bg-[#3D0B37]">
+      <div className="flex justify-between px-[80px] py-[48px]">
+        <div>
+          <div className="text-[#FFDD00] text-[24px] font-['Aclonica'] mb-[30px]">
+            Dazzling Diva
+          </div>
+          <div className="flex gap-[10px] text-white">
+            <img src="/vector-location.png" alt="" />
+            <p>29 SE 2nd Ave, Miami Florida 33131, United States</p>
+          </div>
+          <div className="flex gap-[10px] text-white mb-[20px]">
+            <img src="/vector-mail.png" alt="" />
+            <p>info@dazzingdiva.com</p>
+            <img src="/vector-copy.png" alt="" />
+          </div>
+          <p className="text-white">(+92) 3942 7879</p>
         </div>
-        <div className="flex gap-[10px] text-white">
-          <img src="/vector-location.png" alt="" />
-          <p>29 SE 2nd Ave, Miami Florida 33131, United States</p>
+        <div className="text-white flex justify-between gap-[90px]">
+          <div>
+            <h2 className="text-[16px] text-[#FFDD00] font-['Geist'] font-[700] font-bold mb-[30px]">
+              Shopping
+            </h2>
+            <p className="font-['Inter'] font-[300] text-[14px]">Wishlist</p>
+            <p className="font-['Inter'] font-[300] text-[14px]">Cart</p>
+            <p className="font-['Inter'] font-[300] text-[14px]">
+              Shop by Category
+            </p>
+          </div>
+
+          <div>
+            <h2 className="text-[16px] text-[#FFDD00] font-['Geist'] font-[700] font-bold mb-[30px]">
+              Information
+            </h2>
+            <p className="font-['Inter'] font-[300] text-[14px]">
+              Track My Order
+            </p>
+            <p className="font-['Inter'] font-[300] text-[14px]">
+              Corporate Enquires
+            </p>
+            <p className="font-['Inter'] font-[300] text-[14px]">FAQs</p>
+          </div>
+
+          <div>
+            <h2 className="text-[16px] text-[#FFDD00] font-['Geist'] font-[700] font-bold mb-[30px]">
+              Account
+            </h2>
+            <p className="font-['Inter'] font-[300] text-[14px]">My Account</p>
+            <p className="font-['Inter'] font-[300] text-[14px]">My Orders</p>
+          </div>
         </div>
-        <div className="flex gap-[10px] text-white">
-          <img src="/vector-mail.png" alt="" />
-          <p>info@dazzingdiva.com</p>
-          <img src="/vector-copy.png" alt="" />
-        </div>
-        <p className="text-white">(+92) 3942 7879</p>
       </div>
-      <div className="text-white flex justify-between gap-[90px]">
-        <div>
-          <h2 className="text-[16px] text-[#FFDD00] font-['Geist'] font-[700] font-bold mb-[30px]">
-            Shopping
-          </h2>
-          <p className="font-['Inter']">Whishlist</p>
-          <p className="font-['Inter']">Cart</p>
-          <p className="font-['Inter']">Shop by Category</p>
+      <div className="border-t">
+        <div className="text-white font-['Inter'] text-[16px] font-semibold font-[600]">
+          Copyright &copy; <span className="text-[#FF5F39]">Orbixon</span> 2026.
+          All rights reserved.
         </div>
-
-        <div>
-          <h2 className="text-[16px] text-[#FFDD00] font-['Geist'] font-[700] font-bold mb-[30px]">
-            Information
-          </h2>
-          <p className="font-['Inter']">Track My Order</p>
-          <p className="font-['Inter']">Corporate Enquires</p>
-          <p className="font-['Inter']">FAQs</p>
+        <div className="flex justify-between gap-[50px]">
+          <img src="/facebook.png" alt="" />
+          <img src="/twitter.png" alt="" />
+          <img src="/instagram.png" alt="" />
         </div>
-
-        <div>
-          <h2 className="text-[16px] text-[#FFDD00] font-['Geist'] font-[700] font-bold mb-[30px]">
-            Account
-          </h2>
-          <p className="font-['Inter']">My Account</p>
-          <p className="font-['Inter']">My Orders</p>
-        </div>
+        <div> </div>
       </div>
     </footer>
   );
