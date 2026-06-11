ea7e26e4a52dbc003ebdcf79f52d36f87cf45feb
Habibulah Sirat <63021173+habibullahsirat@users.noreply.github.com>
2026-06-11 17:05:27 +0600

    Topbar created.

---
+++ app/layouts/shared/TopBar.tsx
@@ -0,0 +1,16 @@
+export default function TopBar() {
+  return (
+    <div className="bg-[#3D0B37] flex justify-center text-[white] items-center">
+      <span>Get   </span>
+      <span className="text-[#FFDD00] font-[400]">30% OFF</span>
+      <span>your first order</span>
+      <span className="text-[#FFDD00] font-[400]"> 30% OFF</span>
+      <span>
+        <img src="/star1.png" alt="" />
+      </span>
+      <span>Order now and get a</span>
+      <span className="text-[#FFDD00]">Free</span>
+      <span>delivery Inside Dhaka city.</span>
+    </div>
+  );
+}
