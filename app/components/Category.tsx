5bdadbe14fa7826771790b272a70b790eee319d7
Nahian Noureen <nahiannoureen3@gmail.com>
2026-06-12 14:14:05 +0600

    Category section added.

---
+++ app/components/Category.tsx
@@ -0,0 +1,15 @@
+export default function Category() {
+  return (
+    <div className="bg-[#F2F4F8]">
+      <div className="bg-[#FFFFFF]">
+        <div className="flex">
+          <p className="font-['Geist'] font-[500] text-[24px]">Category</p>
+          <p className="font-['Instrument Serif'] font-[400] font-italic">
+            Section
+          </p>
+        </div>
+        <button>Explore More</button>
+      </div>
+    </div>
+  );
+}
