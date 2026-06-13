c6e93efe9afbac3f27da15d5ff946cce109f9c61
Habibulah Sirat <63021173+habibullahsirat@users.noreply.github.com>
2026-06-14 01:16:34 +0600

    New Arrivals section added.

--- app/welcome/DazzlingDiva.tsx
+++ app/welcome/DazzlingDiva.tsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero";
+import NewArrivals from "../components/NewArrivals";
 
 export function DazzlingDiva() {
   return (
     <div className="px-[84px] py-[48px]">
       <Hero />
+      <NewArrivals />
     </div>
   );
 }
